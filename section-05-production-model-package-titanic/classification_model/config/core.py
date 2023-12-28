61cba80e3c92314e135bcc2a7f337d21e1c0d016
Gabriel Ferreira <gabrielvictor.gferreira@gmail.com>
2023-12-27 20:28:33 -0600

    config/core.py finilized

--- section-05-production-model-package-titanic/classification_model/config/core.py
+++ section-05-production-model-package-titanic/classification_model/config/core.py
@@ -4,10 +4,10 @@ from typing import Dict, List, Sequence
 from pydantic import BaseModel
 from strictyaml import YAML, load
 
-import regression_model
+import classification_model
 
 # Project Directories
-PACKAGE_ROOT = Path(regression_model.__file__).resolve().parent
+PACKAGE_ROOT = Path(classification_model.__file__).resolve().parent
 ROOT = PACKAGE_ROOT.parent
 CONFIG_FILE_PATH = PACKAGE_ROOT / "config.yml"
 DATASET_DIR = PACKAGE_ROOT / "datasets"
@@ -20,8 +20,7 @@ class AppConfig(BaseModel):
     """
 
     package_name: str
-    training_data_file: str
-    test_data_file: str
+    raw_data_file: str
     pipeline_save_file: str
 
 
@@ -32,27 +31,13 @@ class ModelConfig(BaseModel):
     """
 
     target: str
-    variables_to_rename: Dict
-    features: List[str]
-    test_size: float
+    unused_fields: Sequence[str]
+    features: Sequence[str]
+    test_zize: float
     random_state: int
-    alpha: float
-    categorical_vars_with_na_frequent: List[str]
-    categorical_vars_with_na_missing: List[str]
-    numerical_vars_with_na: List[str]
-    temporal_vars: List[str]
-    ref_var: str
-    numericals_log_vars: Sequence[str]
-    binarize_vars: Sequence[str]
-    qual_vars: List[str]
-    exposure_vars: List[str]
-    finish_vars: List[str]
-    garage_vars: List[str]
+    numerical_vars: Sequence[str]
     categorical_vars: Sequence[str]
-    qual_mappings: Dict[str, int]
-    exposure_mappings: Dict[str, int]
-    garage_mappings: Dict[str, int]
-    finish_mappings: Dict[str, int]
+    cabin_vars: Sequence[str]
 
 
 class Config(BaseModel):
